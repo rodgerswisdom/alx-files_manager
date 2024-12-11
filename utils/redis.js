813fa1526e4bc02505f10aea8cc8201ddf7f6792
rodgerswisdom <130440506+rodgerswisdom@users.noreply.github.com>
2024-12-11 21:50:57 +0300

    redis.js

--- utils/redis.js
+++ utils/redis.js
@@ -1,9 +1,6 @@
-// const express = require('express');
-// const redis = require('redis');
 import express from 'express';
-// import redis from 'redis';
 import { creatClient } from 'redis';
-
+mport { promisify } from 'util';
 
 class RedisClient {
   constructor() {
@@ -24,28 +21,17 @@ class RedisClient {
       return client.connected
   }
 
-
-  // Set a value in Redis with an expiration time (in seconds)
-    async set(key, value, expiration = 60) {
-        try{
-            await this.client.setex(key, expiration, JSON.stringify(value));
-        } catch(err){
-        console.error(`Failed to set key: ${key} in Redis: ${err}`);
-      }
+    
+    async set(key, value, expiration){
+        const asyncSet = promisify(this.client.set).bind(client);
+        const value =  await asyncSet(key, value, 'EX', expiration);
   }
 
   // Get a value from Redis
   get(key) {
-    return new Promise((resolve, reject) => {
-      this.client.get(key, (err, data) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(data ? JSON.parse(data) : null);
-        }
-      });
-    });
-  }
+      const asyncGet = promisify(this.redisClient.get).bind(this.redisClient);
+      const value = await asyncGet(key);
+      return value;
 }
 
 // Instantiate Redis client
